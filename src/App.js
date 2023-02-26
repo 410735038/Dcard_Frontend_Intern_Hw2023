e3990532adca6e514d874e8e1dd27f6d0f9b39c7
Nene <410735038@gms.ndhu.edu.tw>
2023-02-27 00:42:59 +0800

    branch整理中

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import "./App.css";
 
+const CLIENT_ID = "118237d1625ed4d4c851";
 function App() {
+  useEffect(() => {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const codeParam = urlParams.get("code");
+    console.log(codeParam);
+  }, []);
+  const loginGithub = () => {
+    window.location.assign(
+      `https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}`
+    );
+  };
   return (
     <div>
       <header>
-        
+        <button onClick={loginGithub}>Login</button>
       </header>
     </div>
   );
