734e57cdab12de51bae200994ecb5b111aedaae2
neorangene <79491377+410735038@users.noreply.github.com>
2023-02-27 19:15:07 +0800

    proxy server
    proxy server 來去得前端 request 發送給 github，以取得 token

---
+++ server.js
@@ -0,0 +1,53 @@
+var express = require("express");
+var cors = require("cors");
+const fetch = (...args) =>
+  import("node-fetch").then(({ default: fetch }) => fetch(...args));
+var bodyParser = require("body-parser");
+
+const CLIENT_ID = "118237d1625ed4d4c851";
+const CLIENT_SECRET = "d1f903b5ef56be86d91072ed2a550bf5edee7b51";
+
+var app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get("/getAccessToken", async function (req, res) {
+  // req.query.code;
+  const params = `?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&code=${req.query.code}`;
+  await fetch(`https://github.com/login/oauth/access_token${params}`, {
+    method: "POST",
+    headers: {
+      "Accept": "application/json",
+    },
+  })
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      console.log(data);
+      res.json(data);
+    //   return res.json(data);
+    });
+});
+
+app.get("/getUserData", async function (req, res) {
+  req.get("Authorization");
+  await fetch("https://api.github.com/user", {
+    method: "GET",
+    headers: {
+      Authorization: req.get("Authorization"),
+    },
+  })
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      console.log(data);
+      res.json(data);
+    });
+});
+
+app.listen(4000, function () {
+  console.log("running");
+});
